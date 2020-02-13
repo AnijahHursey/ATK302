f8a2a05362454a4daf6eb55d85a5cd08f7b1474b
AnijahHursey <54365610+AnijahHursey@users.noreply.github.com>
2020-02-13 11:39:44 -0600

    newbie

---
+++ p5/Week5/timer/sketch.js
@@ -0,0 +1,17 @@
+var timer = 200;
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+  background('purple');
+}
+
+function draw() {
+  // put drawing code here
+  //text(timer, width/2, height/2) ;
+  timer = timer - 1 ;
+  if (timer <= 0) {
+    background(random(256), random(256),random(256),) ;
+    timer = 200 ;
+  }
+}
