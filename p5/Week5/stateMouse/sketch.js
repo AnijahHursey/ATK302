50a5020e1be99202bb8fcebf3380f4ce21896282
AnijahHursey <54365610+AnijahHursey@users.noreply.github.com>
2020-02-13 11:16:25 -0600

    assignment time.

--- p5/Week5/stateMouse/sketch.js
+++ p5/Week5/stateMouse/sketch.js
@@ -1,50 +1,50 @@
-var myState = 0 ;
+var myState = 0;
 
 function setup() {
   // put setup code here
-  createCanvas(800, 800) ;
+  createCanvas(800, 800);
 }
 
 function draw() {
   // put drawing code here
-  switch(myState){
-      case 0:
-      background('red') ;
+  switch (myState) {
+    case 0:
+      background('red');
       text("Hello State 0!", 100, 100);
-      break ;
+      break;
 
-      case 1:
-      background('blue') ;
+    case 1:
+      background('blue');
       text("Hello State 1!", 100, 100);
-      break ;
+      break;
 
-      case 2:
-      background ('green') ;
+    case 2:
+      background('green');
       text("Hello State 2!", 100, 100);
-      break ;
+      break;
 
-      case 3:
-      background ('yellow') ;
+    case 3:
+      background('yellow');
       text("Hello State 3!", 100, 100);
-      break ;
+      break;
 
-      case 4:
-      background ('orange') ;
+    case 4:
+      background('orange');
       text("Hello State 4!", 100, 100);
-      break ;
+      break;
 
-      case 5:
-      background ('purple') ;
+    case 5:
+      background('purple');
       text("Hello State 5!", 100, 100);
-      break ;
+      break;
 
   }
 
 }
 
 function mouseReleased() {
-  myState = myState + 1 ;
-  if (myState > 5){
+  myState = myState + 1;
+  if (myState > 5) {
     myState = 0;
   }
 }
