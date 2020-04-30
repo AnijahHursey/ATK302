0d3e82b024c46e79e63a49732510b6ef13a3c37e
AnijahHursey <54365610+AnijahHursey@users.noreply.github.com>
2020-04-30 16:08:33 -0500

    work both of you

--- p5/Week14/weather/sketch.js
+++ p5/Week14/weather/sketch.js
@@ -27,9 +27,9 @@ function setup() {
 function gotData(data) {
   weather = data;
   console.log(data); // for debugging purposes, print out the JSON data when we get it.
-  ws = weather.wind.speed ;
-  temp = weather.main.temp ;
-  hum = weather.main.humidity ;
+  ws = weather.wind.speed;
+  temp = weather.main.temp;
+  hum = weather.main.humidity;
 }
 
 
@@ -37,59 +37,59 @@ function draw() {
   switch (myState) {
     case 0:
       if (weather) {
-          myState = 1;
+        myState = 1;
       }
       break;
 
     case 1:
-    background('lightblue') ;
-    fill('yellow');
-    ellipse(50, 100, 50, 50);
-    fill('black');
-    text("we have weather", 20, 220) ;
-// parse the weather object and put some text or images using at least 3 different weather data!
-text('The temperature is ' + weather.main.temp, 20, 240);
-text('The Humidity is ' + weather.main.humidity, 20, 260);
-text('The Windspeed is ' + weather.wind.speed, 20, 280);
+      background('lightblue');
+      fill('yellow');
+      ellipse(50, 100, 50, 50);
+      fill('black');
+      text("we have weather", 20, 220);
+      // parse the weather object and put some text or images using at least 3 different weather data!
+      text('The temperature is ' + weather.main.temp, 20, 240);
+      text('The Humidity is ' + weather.main.humidity, 20, 260);
+      text('The Windspeed is ' + weather.wind.speed, 20, 280);
 
-var color = 0;
-color = map(hum, 0, 200, 2, height-10);
+      var color = 0;
+      color = map(hum, 0, 200, 2, height - 10);
 
-noStroke();
-fill(color);
-ellipse(a, 110, 40, 40);
-fill(color);
-ellipse(b, 100, 50, 40);
-fill(color);
-ellipse(c, 115, 80, 40);
-fill(color);
-ellipse(d, 120, 60, 30);
-a = a + ws/16;
-if (a > width) a = 0 ;
-b = b + ws/16;
-if (b > width) b = 0 ;
-c = c + ws/16;
-if (c > width) c = 0 ;
-d = d + ws/16;
-if (d > width) d = 0 ;
+      noStroke();
+      fill(color);
+      ellipse(a, 110, 40, 40);
+      fill(color);
+      ellipse(b, 100, 50, 40);
+      fill(color);
+      ellipse(c, 115, 80, 40);
+      fill(color);
+      ellipse(d, 120, 60, 30);
+      a = a + ws / 16;
+      if (a > width) a = 0;
+      b = b + ws / 16;
+      if (b > width) b = 0;
+      c = c + ws / 16;
+      if (c > width) c = 0;
+      d = d + ws / 16;
+      if (d > width) d = 0;
 
-fill('green');
-rect(0, 300, 400, 100);
-fill('black');
-rect(width-43, height-15, 35, -300);
-fill('black');
-ellipse(width-25.7, 85, 35, 35);
-fill('black');
-ellipse(width-25, height-25, 45, 45);
-fill('red');
-ellipse(width-25, height-25, 40, 40);
+      fill('green');
+      rect(0, 300, 400, 100);
+      fill('black');
+      rect(width - 43, height - 15, 35, -300);
+      fill('black');
+      ellipse(width - 25.7, 85, 35, 35);
+      fill('black');
+      ellipse(width - 25, height - 25, 45, 45);
+      fill('red');
+      ellipse(width - 25, height - 25, 40, 40);
 
 
 
-fill('red');
-var temperature = 0;
-temperature = map(temp, -10, 200, 5, height-10) ;
-rect(width-40, height-10, 30, -temp*3) ;
+      fill('red');
+      var temperature = 0;
+      temperature = map(temp, -10, 200, 5, height - 10);
+      rect(width - 40, height - 10, 30, -temp * 3);
 
 
       break;
